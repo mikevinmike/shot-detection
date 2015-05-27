3b909f318d1446893ca85958f409f6d2b23302f1
GerhardS <ger.scheiber@gmail.com>
2015-05-27 15:14:15 +0200

    JumpToTime Function
    This function provides the ability to jump to a specific time in the
    video by simply clicking on the time under the thumbnail.
    
    Furtheron I corrected the output of the time under the thumbnails. (the
    minutes weren't rigth)

--- frame.js
+++ frame.js
@@ -12,6 +12,25 @@ Frame.prototype = {
         frameContainer.className = 'frame';
         var imageElement = document.createElement('img');
         var caption = document.createElement('span');
+        caption.onclick = function(){
+            //function to jump to the time in the video by clicking on the time of the thumbnail
+            var formattedTime = caption.innerHTML;
+            var jumpToTime = 0;
+            
+            if(formattedTime.length > 8){
+                var fHours = formattedTime.substr(0,1);
+                var fMinutes = formattedTime.substr(2,2);
+                var fSeconds = formattedTime.substr(4,2);                
+                jumpToTime = ((fHours*60*60)+(fMinutes*60)+fSeconds);
+            } else {
+                var fMinutes = formattedTime.substr(0,1);
+                var fSeconds = formattedTime.substr(2,2);
+                jumpToTime = ((fMinutes*60)+fSeconds);
+            }
+            
+            var video = document.getElementById("video");
+            video.currentTime = jumpToTime;
+        };
         imageElement.src = this.dataURL;
         caption.innerHTML = this.getTimeFormatted();
 
@@ -22,7 +41,7 @@ Frame.prototype = {
     },
     getTimeFormatted: function () {
         var hours = Math.floor(this.time / 60 / 60);
-        var minutes = Math.floor(this.time / 60 - hours);
+        var minutes = Math.floor(this.time / 60 - hours * 60 * 60);
         var secondsRaw = this.time - hours * 60 * 60 - minutes * 60;
         var seconds = Math.floor(secondsRaw);
         var milliseconds = Math.floor((secondsRaw - Math.floor(secondsRaw)) * 1000);
